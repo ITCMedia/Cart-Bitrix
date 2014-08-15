0b92164a0d200662b366bce1fb168637906fbc3e
Andrew Zahalski <artlux_studio@mail.ru>
2014-08-15 06:57:19 +0300

    добавлена проверка номера телефона при заказе на международный формат, для смс, для кода PHONE

--- .last_version/install/components/mlife/asz.basket.full/component.php
+++ .last_version/install/components/mlife/asz.basket.full/component.php
@@ -307,13 +307,20 @@ if(intval($ASZ_USER)>0) {
 		$arResult['ORDER_CREATE'] = false;
 		if($_REQUEST['orderfin']==1){
 			//проверка обызятельных полей
-			foreach($arResult["USERPROPS"] as $prop){
+			foreach($arResult["USERPROPS"] as &$prop){
 				if($prop['REQ']=="Y" && $prop["VALUE"]=="") {
 					$arResult['ORDER_ERROR'][] = GetMessage("MLIFE_ASZ_BASKET_FULL_C_ERR4")." ".$prop["NAME"];
 				}elseif($prop['TYPE']=="EMAIL" && $prop['VALUE']){
 					$requlEmail = "/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD";
 					if(!preg_match($requlEmail,$prop['VALUE'])) {
 						$arResult['ORDER_ERROR'][] = GetMessage("MLIFE_ASZ_BASKET_FULL_C_ERR5");
+					}
+				}elseif($prop['CODE']=="PHONE" && $prop['VALUE']){
+					$check = \Mlife\Asz\Functions::checkPhoneNumber($prop['VALUE']);
+					if($check['check']){
+						$prop['VALUE'] = $check["phone"];
+					}else{
+						$arResult['ORDER_ERROR'][] = GetMessage("MLIFE_ASZ_BASKET_FULL_C_ERR_PHONE");
 					}
 				}
 			}
