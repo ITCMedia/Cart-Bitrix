52270c56d9ae779c327897143dfe08466f10f313
Andrew Zahalski <artlux_studio@mail.ru>
2014-08-28 07:44:41 +0300

    Новый шаблон Mlife Asz Super Blue

---
+++ .last_version/install/wizards/mlife/mlife.asz/site/templates/mlife_aszsuper/header.php
@@ -0,0 +1,173 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+	<title><?$APPLICATION->ShowTitle()?></title>
+	<?
+	IncludeTemplateLangFile(__FILE__);
+	global $APPLICATION;
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/jquery.1.10.2.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/jquery.cookie.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/mlfslide.jquery.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/jquery.easing.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/jquery.flexmenu.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/templates.js');
+	$APPLICATION->ShowHead();
+	?>
+</head>
+<body>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+		<div id="site">
+		<div class="mainTopper"><div class="wrap980">
+			<div class="mlfKorz">
+				<?$APPLICATION->IncludeComponent("mlife:asz.basket.small", "super", Array(
+	
+	),
+	false
+);?>
+			</div>
+			<div class="mlfNavMenu">
+				<?
+					$APPLICATION->IncludeComponent("bitrix:menu", "super", Array(
+	"ROOT_MENU_TYPE" => "left",	// Ө殾 嬿 র㯣𮢭󍊉	"MENU_CACHE_TYPE" => "A",	// Ө湨𮢠
+		"MENU_CACHE_TIME" => "36000000",	// ð歿 릸鱮㡭鿠(񥪮)
+		"MENU_CACHE_USE_GROUPS" => "N",	// Է鳻㡲𠯰ᣠ 寱򳯠
+		"MENU_CACHE_GET_VARS" => "",	// ȭḨ󙐠র步塧ᰰﲠ
+		"MAX_LEVEL" => "3",	// ԰㬮禭򨠬殾
+		"CHILD_MENU_TYPE" => "lefto",	// Ө殾 嬿 ﲲᬼ 󰮢
+		"USE_EXT" => "Y",	// Ю嫫򷠲𠴠ꬻ 񠨬殠򘉤࠮򨯟񻙺󩑮u_ext.php
+		"DELAY" => "N",	// ϲ묠弢᳼ 㼯שּׂ殨塸᢫ﮠ 񻗴򲐉"ALLOW_MULTI_SELECT" => "N",	// Ѡ豥𨲼 믫𪮠᫲飭󵠯󭪲省步
+	),
+	false
+);
+					?>
+			</div>
+		</div></div>
+		
+		<div class="wrap">
+			<div class="mlfShap">
+				<div class="leftShapWrap">
+					<div class="mlfLogo">
+						<?
+					$APPLICATION->IncludeComponent("bitrix:main.include", "html_custom", Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_DIR."/include_areas/logo_super.php",
+						"EDIT_TEMPLATE" => "",
+						),
+						false,
+						array(
+						"HIDE_ICONS" => "N"
+						)
+					);
+					?>
+					</div>
+				</div>
+				<div class="rightShapWrap">
+					<div class="blockPhEm">
+						<div class="phone">+7 921 111-11-11</div>
+						<div class="email">info@asz-magazine.ru</div>
+					</div>
+					<div class="blockRight">
+					<div class="zakazMenu">
+						<?
+					$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	"zakaz", 
+	array(
+		"ROOT_MENU_TYPE" => "zakaz",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "36000000",
+		"MENU_CACHE_USE_GROUPS" => "N",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MAX_LEVEL" => "3",
+		"CHILD_MENU_TYPE" => "",
+		"USE_EXT" => "N",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N",
+	),
+	false
+);
+					?>
+					</div>
+					<div class="searchBlock">
+					<?
+							$APPLICATION->IncludeComponent("bitrix:main.include", "html_custom", Array(
+								"AREA_FILE_SHOW" => "file",
+								"PATH" => SITE_DIR."/include_areas/search_block.php",
+								"EDIT_TEMPLATE" => "",
+								),
+								false,
+								array(
+								"HIDE_ICONS" => "N"
+								)
+							);
+							?>
+					</div>
+					</div>
+				</div>
+			</div>
+			<div class="bottomShap">
+				<?
+					$APPLICATION->IncludeComponent("bitrix:menu", "bottomshap", Array(
+	"ROOT_MENU_TYPE" => "bottomshap",	// Ө殾 嬿 র㯣𮢭󍊉	"MENU_CACHE_TYPE" => "A",	// Ө湨𮢠
+		"MENU_CACHE_TIME" => "36000000",	// ð歿 릸鱮㡭鿠(񥪮)
+		"MENU_CACHE_USE_GROUPS" => "N",	// Է鳻㡲𠯰ᣠ 寱򳯠
+		"MENU_CACHE_GET_VARS" => "",	// ȭḨ󙐠র步塧ᰰﲠ
+		"MAX_LEVEL" => "4",	// ԰㬮禭򨠬殾
+		"CHILD_MENU_TYPE" => "lefto",	// Ө殾 嬿 ﲲᬼ 󰮢
+		"USE_EXT" => "Y",	// Ю嫫򷠲𠴠ꬻ 񠨬殠򘉤࠮򨯟񻙺󩑮u_ext.php
+		"DELAY" => "N",	// ϲ묠弢᳼ 㼯שּׂ殨塸᢫ﮠ 񻗴򲐉"ALLOW_MULTI_SELECT" => "N",	// Ѡ豥𨲼 믫𪮠᫲飭󵠯󭪲省步
+	),
+	false
+);
+					?>
+			</div>
+			
+			<div class="contus">
+				<div class="rightBlock">
+					
+					<?$APPLICATION->ShowViewContent('filter');?>
+					<div class="contact"><?
+							$APPLICATION->IncludeComponent("bitrix:main.include", "html_custom", Array(
+								"AREA_FILE_SHOW" => "file",
+								"PATH" => SITE_DIR."/include_areas/right_contact.php",
+								"EDIT_TEMPLATE" => "",
+								),
+								false,
+								array(
+								"HIDE_ICONS" => "N"
+								)
+							);
+							?>
+					</div>
+					<?if($vk){?>
+					<div class="vkWrap">
+					<?
+							$APPLICATION->IncludeComponent("bitrix:main.include", "html_custom", Array(
+								"AREA_FILE_SHOW" => "file",
+								"PATH" => SITE_DIR."/include_areas/vk.php",
+								"EDIT_TEMPLATE" => "",
+								),
+								false,
+								array(
+								"HIDE_ICONS" => "N"
+								)
+							);
+							?>
+					
+					</div>
+					<?}?>
+				</div>
+				<div class="contwp">
+				<div class="operafix">.... ...... ........ ....... ........ ......... .....
+.... ...... ........ ....... ........ ......... .....
+.... ...... ........ ....... ........ ......... .....
+</div>
+					<?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "kroshke", array(
+	"START_FROM" => "0",
+	"PATH" => "",
+	"SITE_ID" => "s3"
+	),
+	false
+);?>
